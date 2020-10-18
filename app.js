39412b71d32df4b223d26bc81449977f47f80179
@Roninanu777 <ronirajkamalpradhan1112@gmail.com>
2020-10-19 00:11:31 +0530

    app module

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+
+// Routers
+const indexRouter = require("./routes/index");
+
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", indexRouter);
+
+module.exports = app;
