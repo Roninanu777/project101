e14859880e4f194c7841583ea346c94cb65a5f7e
@Roninanu777 <ronirajkamalpradhan1112@gmail.com>
2020-10-19 00:11:45 +0530

    initializing

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.sendFile("index.html").status(200);
+    res.send("Hello world");
 });
 
 module.exports = router;
